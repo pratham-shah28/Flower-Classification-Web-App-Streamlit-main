9bb780d4736a5603811470e7f9fdf7df79367330
pratham-shah28 <psshah2803@gmail.com>
2022-11-08 02:06:29 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import tensorflow as tf
+import streamlit as st
+import pathlib
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
+img_height,img_width=180,180
+batch_size=32
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+class_names = train_ds.class_names
+print(class_names)
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('C:\Flower-Classification-Web-App-Streamlit-main\my_model2.hdf5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write("""
+         # Flower Classification
+         """
+         )
+
+file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png",'jpeg'])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def import_and_predict(image_data, model):
+    
+        size = (180,180)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        
+        img_reshape = img[np.newaxis,...]
+    
+        prediction = model.predict(img_reshape)
+        
+        return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    score = tf.nn.softmax(predictions[0])
+    st.write(predictions)
+    st.write(score)
+    
+    st.write(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(predictions)], 100 * np.max(score))
+)
